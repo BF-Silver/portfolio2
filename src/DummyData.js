e8ea884f040cfc1932b6940e80de03280e79f1f3
Hiroaki Fukuda <hirocreative0701@gmail.com>
2024-08-26 00:41:35 +0900

    third

---
+++ src/DummyData.js
@@ -0,0 +1,22 @@
+export const PostsData = [
+  {
+    id: 1,
+    name: "Hiroaki",
+    content: "Fukuda",
+  },
+  {
+    id: 2,
+    name: "none-name",
+    content: "hiroaki-fukuda",
+  },
+  {
+    id: 3,
+    name: " 頑張る",
+    content: "やってやる",
+  },
+  {
+    id: 4,
+    name: "今日のこと",
+    content: "想定内",
+  },
+];
